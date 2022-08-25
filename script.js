a9168a38b7ab3544d0eb91e13eb3cd0777573275
diego87 <87.c.die@gmail.com>
2022-08-25 14:13:00 -0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const listaDeNumeros = [1,2,3,4,5,6,7,8,9]
+
+function lista(numeros,num){
+    let newArr = []
+
+    for(let i = 0; i <numeros.length; i++){
+        if(listaDeNumeros[i] !== num){
+            newArr.push(listaDeNumeros[i])
+        }
+    }
+    return newArr.length
+}
+console.log(lista(listaDeNum,1)
+    
+// for(let i = listaDeNumeros.length; i >listaDeNumeros.length; i--){
+// }
+// console.log(listaDeNumeros)
